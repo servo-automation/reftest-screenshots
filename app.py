5524f35721099c2f6f87c94050209b0f279a2247
Ravi Shankar <wafflespeanut@gmail.com>
2017-09-13 12:46:54 +0530

    Extract screenshots from logs and blend with Pillow

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 from PIL import Image
+from StringIO import StringIO
+from base64 import b64encode, b64decode
 from flask import Flask, Response, request
 from io import BytesIO
-from pyvirtualdisplay import Display
-from selenium.webdriver import Chrome
-from threading import Lock
 
 import contextlib, json, os, time, urllib2
 
@@ -21,24 +20,6 @@ setTimeout(function() {
 </script>
 '''
 
-class Driver(object):
-    def __init__(self):
-        self.lock = Lock()
-        print 'Starting driver...'
-        self.driver = Chrome()
-        self.driver.set_window_position(0, 0)
-        self.driver.set_window_size(1280, 720)
-
-    def take_shot(self, url, png):
-        with self.lock:
-            self.driver.get(url)
-            time.sleep(1000)        # delay for javascript
-            self.driver.get_screenshot_as_file(png)
-
-
-display = Display(visible=0, size=(1280, 720))
-display.start()
-engine = Driver()
 app = Flask(__name__)
 
 
@@ -54,37 +35,44 @@ def index():
     print summary_url
     print log_url
 
-    with contextlib.closing(urllib2.urlopen(summary_url)) as fd_sum, contextlib.closing(urllib2.urlopen(log_url)) as fd_log:
-        fails = map(lambda s: json.loads(s)['test'], filter(lambda s: '"status": "FAIL"' in s, fd_sum.read().split('\n')))
-        result = filter(lambda s: 'screenshot' in s and any(f in s for f in fails), fd_log.read().split('\n'))
+    with contextlib.closing(urllib2.urlopen(summary_url)) as fd_sum:
+        fails = map(lambda s: json.loads(s)['test'],
+                    filter(lambda s: '"status": "FAIL"' in s,
+                           fd_sum.read().split('\n')))
+
+    with contextlib.closing(urllib2.urlopen(log_url)) as fd_log:
+        result = filter(lambda s: 'screenshot' in s and any(f in s for f in fails),
+                        fd_log.read().split('\n'))
         if not result:
-            return 'null'
+            return '{}'
 
-    text = result[0]
+    resp = []
+    for res in result:
+        data = res['extra']['reftest_screenshots']
+        test, ref = data[0], data[1]
+        test_img = Image.open(BytesIO(b64decode(test['screenshot'])))
+        ref_img = Image.open(BytesIO(b64decode(ref['screenshot'])))
+        blend_img = Image.blend(test_img, ref_img, alpha=0.75)
+        buf = BytesIO()
+        blend_img.save(buf, format='PNG')
 
-    analyzer = 'http://hoppipolla.co.uk/410/reftest-analyser-structured.xhtml'
-    modified = 'loaded_%s.xhtml' % build
-    with contextlib.closing(urllib2.urlopen(analyzer)) as fd, open(modified, 'w') as out:
-        html = fd.read()
-        match = '</body>'
-        idx = html.find(match)
-        html = html[:idx] + SCRIPT % text + html[idx:]
-        out.write(html)
-        print 'Modified analyzer.'
+        resp.append({
+            'test': {
+                'url': test['url'],
+                'image': test['screenshot']
+            },
+            'ref': {
+                'url': ref['url'],
+                'image': ref['screenshot']
+            },
+            'blend': b64encode(buf.getvalue()),
+        })
 
-    engine.take_shot('file://' + os.path.abspath(modified),
-                     'result_%s.png' % build)
-
-    elem = engine.driver.find_element_by_id('svg')
-    size, pos = elem.size, elem.location
-    img = Image.open(png)
-    cropped = img.crop((pos['x'], pos['y'], pos['x'] + size['width'], pos['y'] + size['height']))
-    byte_arr = BytesIO()
-    cropped.save(byte_arr, format='PNG')
-    os.remove(png)
-    os.remove(modified)
-
-    return Response(byte_arr.getvalue(), mimetype="image/png")
+    return app.response_class(
+        response=json.dumps(resp),
+        status=200,
+        mimetype='application/json'
+    )
 
 
 port = int(os.environ.get('PORT', 5000))
